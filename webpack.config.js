6088794994fe00174694ed26dec337bf94598d19
Madrid Pepín <jamd@gft.com>
2020-03-03 09:09:19 +0100

    hot reload scss

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,14 @@ const webpack = require('webpack');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-//const HtmlWebpackHotPlugin = require('html-webpack-hot-plugin')
+const HtmlWebpackHotPlugin = require('html-webpack-hot-plugin');
 
 
-const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const htmlHotPlugin = new HtmlWebpackHotPlugin({ hot: true,});
 
 
 
@@ -21,6 +24,7 @@ module.exports = {
   },
   devtool: 'inline-source-map',
   watch: true,
+  
 
   plugins: [
     new CleanWebpackPlugin(),
@@ -33,31 +37,35 @@ module.exports = {
         encabezamiento: 'Webpack',
       }
     }),
-    //new MiniCSSExtractPlugin({filename: "css/style.css",  chunkFilename: "[id].css"}),
+    htmlHotPlugin,
+    new MiniCssExtractPlugin({filename: "css/style.css"}),
+    
 
   ],
-  module: {
-    rules: [
-      // {
-      //   test: /\.scss$/,
-      //   loader: [MiniCSSExtractPlugin.loader, 'css-loader', 'sass-loader']
-        
-      // }
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          'style-loader',
-          // Translates CSS into CommonJS
-          'css-loader',
-          // Compiles Sass to CSS
-          'sass-loader'
+ module: {
+    rules: [{
+      test: /\.scss$/,
+      use: [
+          'css-hot-loader',
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+              // options...
+            }
+          }
         ]
-      }
-      
-    ]
+    }]
   },
   devServer: {
+    before(app, server) {
+       htmlHotPlugin.setDevServer(server)
+    },
+    
    
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
